7799eb848edbf8d693bcb92cd6713ff8719886f8
Saud Alshamsi <23ud92@gmail.com>
2019-10-27 21:06:59 +0300

    dgf

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import Movie from './Movie';
 import './App.css';
 
 function App() {
-  return (
+
+return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Movie title="The Fellowship of the Ring" hours="2" minutes="58"></Movie>
+        <Movie title="The Two Towers" hours="2" minutes="59"></Movie>
+        <Movie title="The Return of the King" hours="3" minutes="21"></Movie>
         <a
           className="App-link"
           href="https://reactjs.org"
